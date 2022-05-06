855d98a71019be9eff66d3924fa3c4c5e6cc73c8
TeLe TiPs All Testings <77987544+teletipsalltestings@users.noreply.github.com>
2022-05-06 17:12:59 +0530

    Initial commit

--- date_time_userbot_teletips.py
+++ date_time_userbot_teletips.py
@@ -31,7 +31,7 @@ async def main_teletips():
                 Quotes_teletips = random.choice(quotes_teletips)
                 Emojis_teletips = random.choice(emojis_teletips)
                 TimeZone_teletips = datetime.datetime.now(pytz.timezone(f"{Time_Zone}"))
-                Time_teletips = TimeZone_teletips.strftime("%H:%M %p")
+                Time_teletips = TimeZone_teletips.strftime("%I:%M %p")
                 Date_teletips = TimeZone_teletips.strftime("%b %d") 
                 Image_teletips = Image.open("image.jpg")
                 Image_font_teletips = ImageFont.truetype("ds-digit.ttf", 360)
@@ -42,7 +42,7 @@ async def main_teletips():
                 await Date_Time_Userbot_teletips.update_profile(bio = f"{Emojis_teletips} {Quotes_teletips}" , last_name = f"| ⏰ {Time_teletips} | 📅 {Date_teletips}")
                 await Date_Time_Userbot_teletips.set_profile_photo(photo="Image_final_teletips.jpg")
                 me = await Date_Time_Userbot_teletips.get_me()
-                photos = await Date_Time_Userbot_teletips.get_profile_photos("me")
+                photos = Date_Time_Userbot_teletips.get_chat_photos("me")
                 try:
                     await Date_Time_Userbot_teletips.delete_profile_photos(photos[1].file_id)
                 except Exception:
